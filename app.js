266515fa0aa80c061041ca092bbc00c2cfd52702
FelipeSarriaCarrasco <felipe.s.carrasco@gmail.com>
2025-01-27 20:32:02 -0300

    se agragan la funciones "agregarAmigo" y "BorrarInputs"

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+
+
+
+// funcion que se encarga de agregar los nombres de los amigos, al array.
+function agregarAmigo(){
+    let nombreAgregado = document.getElementById('amigo').value;
+    listaAmigos.push(nombreAgregado);
+    borrarinputs();
+    console.log(listaAmigos);
+};
+
+// Funcion se encarga de borrar el textbox del cuadro de Nonbre ingresado.
+function borrarinputs(){
+    document.querySelector('#amigo').value = '';
+};
