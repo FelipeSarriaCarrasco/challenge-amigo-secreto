1d3c49fb7c990a00630cdd3aac7270cf22d311f7
FelipeSarriaCarrasco <felipe.s.carrasco@gmail.com>
2025-01-28 16:40:34 -0300

    se agraga funcion listar amigos inscritos

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ const reg = new RegExp(/([0-9])|\W/g);
 // funcion que se encarga de agregar los nombres de los amigos, al array.
 function agregarAmigo(){
     let nombreAgregado = document.getElementById('amigo').value;
-    validaTexto(nombreAgregado)
-    (validaEntradasTexto(nombreAgregado) == true ? listaAmigos.push(nombreAgregado) : alert('Debe ingresar un nombre válido'));
+    (validaTexto(nombreAgregado)) == true ? listaAmigos.push(nombreAgregado) : alert('Debe ingresar un nombre válido');
     borrarinputs();
-    console.log(listaAmigos);
+    listarAmigos('listaAmigos',nombreAgregado);
 };
 
 // Funcion se encarga de borrar el textbox del cuadro de Nonbre ingresado.
@@ -22,13 +21,20 @@ function borrarinputs(){
 
 // funcion revisa las expresiones regulare para determinar si el texto ingresado no es simbolo - espacios o numeros
 function validaTexto(textoIngresado){
-    let valorResultado = false
     if (reg.test(textoIngresado)) {
         return false
         //console. log(`Error codigo: ${textoIngresado} el error ${reg.test(textoIngresado)}`);
     }else if (!reg.test(textoIngresado)) {
         return true
         //console. log(`Codigo correcto: ${textoIngresado} esta bien ${reg.test(textoIngresado)}`);
-    }
+    };
 };
+
+//funcion listar amigos inscritos
+function listarAmigos(elemento, texto) {
+    let elementoHTML = document.getElementById(elemento)
+    elementoHTML.innerHTML += "<li>"+texto+"</>";
+    return;
+};
+
 
