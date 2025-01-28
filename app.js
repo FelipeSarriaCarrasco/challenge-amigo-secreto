250eb2f7380dff1347cd4f17c5bd72ac6f2a9102
FelipeSarriaCarrasco <felipe.s.carrasco@gmail.com>
2025-01-27 22:56:45 -0300

    se agrega la funcion que valida el Texto

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let listaAmigos = [];
 // funcion que se encarga de agregar los nombres de los amigos, al array.
 function agregarAmigo(){
     let nombreAgregado = document.getElementById('amigo').value;
-    listaAmigos.push(nombreAgregado);
+    (validaEntradasTexto(nombreAgregado) == true ? listaAmigos.push(nombreAgregado) : alert('Debe ingresar un numero valido'));
+    //listaAmigos.push(nombreAgregado); // se guardan los nombre de los amigos en el Array
     borrarinputs();
     console.log(listaAmigos);
 };
@@ -15,4 +16,25 @@ function agregarAmigo(){
 // Funcion se encarga de borrar el textbox del cuadro de Nonbre ingresado.
 function borrarinputs(){
     document.querySelector('#amigo').value = '';
+};
+
+
+// funcion se encarga de verificar los caracteres del texto ingresado mediante el valor ASCII que posee cada valor
+function validaEntradasTexto(textoIngresado){
+    let valorResultado = false
+    for (let tx = 0; tx < textoIngresado.length; tx++) {
+        let elemento = textoIngresado.charAt(tx); // se extare cada caracter de izquier a derecha
+
+        // charCodeAt() devuelve el valor ASCII del caracter
+        // los numeros ASCII entre el 65 y 90 corresponden a las letras Mayusculas
+        // los numero ASCII entre el 97 y 122 corresponden a las letras Minusculas
+        if (elemento.charCodeAt(0) >= 65 && elemento.charCodeAt(0) <= 90) {
+            valorResultado = true
+        }else if (elemento.charCodeAt(0) >= 97 && elemento.charCodeAt(0) <= 122) {
+            valorResultado = true
+        }else{
+            return false
+        }
+    }
+    return valorResultado;
 };
